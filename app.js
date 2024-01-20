c7bc9ff3f1871365043e1ccbb5864e2baeb682db
alexsatm <dzamilovaalbina2@gmail.com>
2024-01-20 22:00:27 +0300

    feat: add calculator click

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const calculator = document.querySelector('.calculator');
+const history = [];
+let tempNumber = '';
+let operationType;
 
 calculator.addEventListener('click', (e) => {
     const target = e.target;
     if (target.classList.contains('col')) {
-        console.log('col');
+        const data = target.dataset.type;
+        operation(data);
+        console.log(tempNumber);
     }
 })
